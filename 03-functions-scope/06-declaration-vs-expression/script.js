278a8761415ba7ac5f146efaa245ee8ee2950c1b
Alexeencokirill <97600028+Alexeencokirill@users.noreply.github.com>
2023-08-10 00:50:32 +0200

    declaration&expretion

---
+++ 03-functions-scope/06-declaration-vs-expression/script.js
@@ -0,0 +1,16 @@
+// Function Declaration
+
+function addDolarSign(value) {
+    return '$' + value;
+}
+
+console.log(addDolarSign(100))
+
+// Function Expression
+
+const addPlusSign = function(value) {
+    return '+' + value
+};
+
+console.log(addPlusSign(50))
+
