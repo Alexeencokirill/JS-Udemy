8cd741e30327f84ea0f0e373817547ff10e13a82
Alexeencokirill <97600028+Alexeencokirill@users.noreply.github.com>
2023-08-14 22:23:03 +0200

    if-statements

---
+++ 04-logic-control-flow/01-if-statements/script.js
@@ -0,0 +1,30 @@
+if (true) {
+    console.log('This is true')
+}
+
+if (false) {
+    console.log('This is not true')
+}
+
+const x = 10;
+const y = 5;
+
+if (x > y) {
+    console.log(`${x} is greater then ${y}`)
+}
+
+if (x === y) {
+    console.log(`${x} is equal to ${y}`)
+} else {
+    console.log(`${x} is  NOT equal to ${y}`)
+}
+
+if (x !== y) {
+    const z = 20;
+    console.log(`${z} is 20`)
+}
+
+// Short hand IF
+
+if (x >= y) console.log(`${x} is greater than or equal to ${y}`)
+else console.log(`This is false`)
