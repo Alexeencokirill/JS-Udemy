17aa2d44321f48057a8fabdc07185b3c8017b3be
Alexeencokirill <97600028+Alexeencokirill@users.noreply.github.com>
2023-08-20 21:26:05 +0200

    for-loop

--- 05-iteration-array-methods/01-for-loop/script.js
+++ 05-iteration-array-methods/01-for-loop/script.js
@@ -5,3 +5,38 @@
 // CONDITION EXPRESSION - Condition that the loop will continue to run as long as it is met or until the condition is false
 // INCREMENT EXPRESSION - Expression that will be executed after each iteration of the loop. Usually increments the variable
 // STATEMENT - Code that will be executed each time the loop is run. To execute a `block` of code, use the `{}` syntax
+
+// ==================== Siple for loop ====================
+
+// for (let i = 0; i <= 10; i++) {
+//     if (i === 7) {
+//         console.log(`${i} is my lucky number!`)
+//     } else {
+//         console.log('Number ' + i)
+//     }  
+// }
+
+// ==================== Nested loops ====================
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log('Number' + i)
+
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`${i} * ${j} = ${i * j}`)
+//     }
+// }
+
+// ==================== Loop throught an array ====================
+
+const names = ['Kirill', 'Max', 'Vita', 'Dema'];
+
+ for (let i = 0; i < names.length; i++) {
+    if (i === 2) {
+        console.log(names[i] + ' is the best')
+    } else {
+        console.log(names[i]) 
+    }
+    // if (names.[i] === 'Vita) {
+    //     console.log(`${names.[i]} is the best`)
+    // }
+ }
