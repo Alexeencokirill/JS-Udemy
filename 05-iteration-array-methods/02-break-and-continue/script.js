15edb85deb4a680dc079ca2c13df16bd91a85d55
Alexeencokirill <97600028+Alexeencokirill@users.noreply.github.com>
2023-08-23 22:12:22 +0200

    break&continue

---
+++ 05-iteration-array-methods/02-break-and-continue/script.js
@@ -0,0 +1,19 @@
+// Break 
+
+// for (let i = 1; i <= 20; i++) {
+//     if(i === 15) {
+//         console.log('Breaking...')
+//         break;
+//     }
+//     console.log(i)
+// }
+
+// Continue
+
+ for (let i = 1; i <=20; i++) {
+    if (i === 13) {
+        console.log('Skipping...')
+        continue;
+    }
+    console.log(i)
+ }
