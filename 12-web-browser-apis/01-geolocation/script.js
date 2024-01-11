b82cc5fc6b7ef59c1f7d496a1dc7093fc33b76f7
Alexeencokirill <97600028+Alexeencokirill@users.noreply.github.com>
2024-01-11 18:33:02 +0100

    geolocation

--- 12-web-browser-apis/01-geolocation/script.js
+++ 12-web-browser-apis/01-geolocation/script.js
@@ -1,4 +1,39 @@
 // getCurrentPosition()
+function curSuccess(pos) {
+    const coords = pos.coords;
+console.log(`Latitude: ${coords.latitude}`);
+console.log(`Longitude: ${coords.longitude}`);
+console.log(`Within: ${coords.accuracy} meters`)
+}
+
+function curError(err) {
+    console.log(`Error: ${err.code} - ${err.message}`)
+}
+
+const curOptions = {
+    enableHghAccuracy: true, // Use gps if available
+};
+
+navigator.geolocation.getCurrentPosition(curSuccess, curError, 
+    curOptions);
 
 
 // watchPosition()
+
+function watchSuccess(pos) {
+    const coords = pos.coords;
+console.log(`Latitude: ${coords.latitude}`);
+console.log(`Longitude: ${coords.longitude}`);
+console.log(`Within: ${coords.accuracy} meters`)
+}
+
+function watchError(err) {
+    console.log(`Error: ${err.code} - ${err.message}`)
+}
+
+const watchOptions = {
+    enableHghAccuracy: true, // Use gps if available
+};
+
+navigator.geolocation.watchCurrentPosition(watchSuccess, watchError, 
+    watchOptions);
